7622034cd39a02f3a801df3b21573371b5961ff0
Cyber1ord <rooda43@gmail.com>
2023-05-11 06:30:13 -0700

    a program that prints #pythoniscool, followed by a new line, in the standard output.

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,48 +1,42 @@
 #include "lists.h"
-
 /**
- * insert_node - inserts a new node at a given position
- * @head: pointer to a pointer to the head of the linked list
- * @number: value to be added to the new node
- *
- * Return: address of the new node, or NULL if it fails
+ * insert_node - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @number: index of the list where the new node is
+ * added.
+ * Return: the address of the new node, or NULL
+ * Aladin Bensassi
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node, *current_node, *prev_node = NULL;
-
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	listint_t *new;
+	listint_t *h;
+	listint_t *h_prev;
+	h = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
-
-	new_node->n = number;
-	new_node->next = NULL;
-
+	while (h != NULL)
+	{
+		if (h->n > number)
+			break;
+		h_prev = h;
+		h = h->next;
+	}
+	new->n = number;
 	if (*head == NULL)
 	{
-		*head = new_node;
-		return (new_node);
-	}
-
-	current_node = *head;
-
-	while (current_node != NULL && current_node->n < number)
-	{
-		prev_node = current_node;
-		current_node = current_node->next;
-	}
-
-	if (prev_node == NULL)
-	{
-		new_node->next = *head;
-		*head = new_node;
+		new->next = NULL;
+		*head = new;
 	}
 	else
 	{
-		new_node->next = prev_node->next;
-		prev_node->next = new_node;
+		new->next = h;
+		if (h == *head)
+			*head = new;
+		else
+			h_prev->next = new;
 	}
-
-	return (new_node);
+	return (new);
 }
-
