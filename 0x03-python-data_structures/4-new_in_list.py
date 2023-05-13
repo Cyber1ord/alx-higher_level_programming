95b8c6a9dcdc91a74f8ea1f781852b9ae02b9f5c
Cyber1ord <rooda43@gmail.com>
2023-05-13 21:41:27 +0000

    C function that prints some basic info about Python lists.

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    length = len(my_list)
-
-    if idx < 0 or (idx > length - 1):
-        return (my_list)
-
-    my_list[idx] = element
-    new_list = my_list
-    return (my_list)
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list[:]
+    else:
+        return my_list[:idx] + [element] + my_list[idx+1:]
